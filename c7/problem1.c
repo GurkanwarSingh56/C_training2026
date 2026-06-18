fadf1c07628d4fe54567a9c33e949a9e6b593123
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2026-06-18 15:03:27 +0530

    Add problem 1 solution

---
+++ c7/problem1.c
@@ -0,0 +1,14 @@
+//Q1. Write a C program to enter a number and print its reverse.
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("enter the no");
+    scanf("%d",&i);
+    while (n==0)
+    {
+       n= i%10;
+       printf("%d",n);
+       
+    }
+    
+}
