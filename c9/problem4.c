512a3aa95548f49323a8df5b6c9c1bd7bb6c054e
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2026-06-20 13:41:24 +0530

    Add problem 4 solution

---
+++ c9/problem4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+    int arr[5];
+
+    for(int i=0; i<5; i++)
+        scanf("%d", &arr[i]);
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i=1; i<5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d", min);
+
+    return 0;
+}
