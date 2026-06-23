ac65853cd0b591670dd4aef0b71fb635cedba2f4
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2026-06-23 10:16:13 +0530

    Add problem 4 solution

---
+++ c11/problem4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], sub[3][3];
+
+    printf("Enter first matrix:\n");
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            scanf("%d",&b[i][j]);
+
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            sub[i][j]=a[i][j]-b[i][j];
+
+    printf("Subtraction Matrix:\n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++)
+            printf("%d ",sub[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
